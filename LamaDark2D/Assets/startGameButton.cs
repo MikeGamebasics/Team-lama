72ec4788412b5867dba072c6568453eb0d81db52
Wietske Overtoom <wietske@gamebasics.nl>
2019-02-19 15:44:44 +0100

    debug tekstje op knop werkt

---
+++ LamaDark2D/Assets/startGameButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class startGameButton : MonoBehaviour
+{
+    public void ShowDebug()
+    {
+        Debug.Log("You clicked a button");
+    }
+}
