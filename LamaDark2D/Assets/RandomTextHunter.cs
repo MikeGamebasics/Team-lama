27450883afc08e1c99c2c656ed43dbfae2594c37
Wietske Overtoom <wietske@gamebasics.nl>
2019-02-21 09:07:11 +0100

    Trash Talk scriptje toegevoegd

---
+++ LamaDark2D/Assets/RandomTextHunter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTextHunter : MonoBehaviour
+{
+    public float showTextDuration;
+    public float intervalBetweenTexts;
+    float intervalSinceLastUpdate;
+    public static string randomHunterText;
+
+    //insert random text here
+    string[] randomHunterTexts = { "What are you doing?", "Git Gud", "you weakling", "You can't defeat me!", "Is this all?", "Nani, the Fuck?" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AppointRandomText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        intervalSinceLastUpdate += Time.deltaTime;
+        if (intervalSinceLastUpdate >= intervalBetweenTexts)
+        {
+            intervalSinceLastUpdate -= intervalBetweenTexts;
+            Invoke("AppointRandomText", showTextDuration);
+            GetComponent<TextMesh>().text = null;
+        }
+
+    }
+
+    void AppointRandomText()
+    {
+        //gets a random index number
+        int randomIndex = Mathf.FloorToInt(Random.Range(0, randomHunterTexts.Length));
+        randomHunterText = randomHunterTexts[randomIndex];
+
+        //shows text above player. N is needed so it is indeed above the player
+        GetComponent<TextMesh>().text = randomHunterText + "\n\n\n\n\n";
+    }
+
+
+}
